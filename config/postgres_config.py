a362afad846a0cee746021ff7ef9fdd0355de253
advaccaro <avaccaro@usc.edu>
2018-06-28 21:33:45 -0700

    porting db to postgres

---
+++ config/postgres_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys, os, psycopg2
+
+MINTCAST_PATH = os.environ.get('MINTCAST_PATH')
+
+hostname = 'jonsnow.usc.edu'
+username = 'vaccaro'
+password = 'm\\tC7;cc'
+database = 'mintcast'
+
+conn = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
